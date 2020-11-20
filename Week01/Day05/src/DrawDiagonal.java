82f50ddcf10505e4a7389357e7f70a9873070de9
Marcell Borsos <marcell.borsos@yahoo.com>
2020-11-20 13:51:56 +0100

    Added DrawDiagonal.java to Week01/Day05

---
+++ Week01/Day05/src/DrawDiagonal.java
@@ -0,0 +1,30 @@
+public class DrawDiagonal {
+    public static void main(String[] args) {
+        // Write a program that reads a number from the standard input, then draws a
+        // square like this:
+        //
+        //
+        // %%%%%
+        // %%  %
+        // % % %
+        // %  %%
+        // %%%%%
+        //
+        // The square should have as many lines as the number was
+        int a = 7;
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < a; j++) {
+                if (j == i) {
+                    System.out.print("%");
+                } else if (j == 0 || j == a - 1) {
+                    System.out.print("%");
+                } else if (i == 0 || i == a - 1) {
+                    System.out.print("%");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
